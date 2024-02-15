c3b3015e635ac3e0d2cf68f49be794a018eac9bb
santiagohirsch <shirsch@itba.edu.ar>
2024-02-15 13:34:07 -0300

    Arrays sumAll

--- Arrays/sum.go
+++ Arrays/sum.go
@@ -1,6 +1,6 @@
 package arrays
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 
 	for _, number := range numbers {
@@ -8,4 +8,13 @@ func Sum(numbers [5]int) int {
 	}
 
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
 }
