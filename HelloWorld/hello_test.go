56482087174cda2fc3d5f86dea150588bf2505f4
santiagohirsch <shirsch@itba.edu.ar>
2024-02-15 12:21:48 -0300

    Hello World Test subtests + assertion refactor

--- HelloWorld/hello_test.go
+++ HelloWorld/hello_test.go
@@ -3,9 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Santi")
-	want := "Hello Santi!"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Santi")
+		want := "Hello Santi!"
+		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("empty string defaults to 'World'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello World!"
+		assertCorrectMessage(t, got, want)
+	})
+
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
